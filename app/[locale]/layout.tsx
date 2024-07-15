f2a7a2c6e5f69b2667068e566fdb15cd74827a02
Jacky Randrianarivony <jlrandrianarivony@gmail.com>
2024-07-15 19:47:51 +0300

    feat: insight analytique

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -4,12 +4,14 @@ import { Montserrat } from 'next/font/google'
 import './globals.css'
 import Provider from '@/app/components/providers/provider'
 import BaseLayout from '@/app/components/layout/layout'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Montserrat({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Jacky R. - Next.js Developer & Cloud Enthusiast',
-  description: 'Explore the portfolio of Jacky Randrianarivony, a skilled web developer from Antananarivo, Madagascar. Specializing in Next.js, AWS, and Tailwind CSS, Jacky delivers innovative and efficient web solutions. Discover projects that showcase expertise in web development, cloud services, and automation.',
+  description:
+    'Explore the portfolio of Jacky Randrianarivony, a skilled web developer from Antananarivo, Madagascar. Specializing in Next.js, AWS, and Tailwind CSS, Jacky delivers innovative and efficient web solutions. Discover projects that showcase expertise in web development, cloud services, and automation.',
 }
 
 export default function RootLayout({
@@ -25,6 +27,7 @@ export default function RootLayout({
         <Provider>
           <BaseLayout>{children}</BaseLayout>
         </Provider>
+        <Analytics />
       </body>
     </html>
   )
