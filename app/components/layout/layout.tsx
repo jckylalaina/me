4a7035400bd2b16dd1ceca2cecae6060e1fdc7a6
jckylalaina <jlrandrianarivony@gmail.com>
2025-03-28 15:22:58 +0300

    fix: overflow

--- app/components/layout/layout.tsx
+++ app/components/layout/layout.tsx
@@ -19,14 +19,14 @@ export default function BaseLayout({
 }) {
   return (
     <main className='dark min-h-screen bg-background  mx-auto  text-secondary relative max-w-[2000px]'>
-      <div className='max-w-screen-4xl flex flex-col-reverse mx-auto px-5 xl:px-20'>
+      <div className='max-w-screen-4xl flex flex-col-reverse mx-auto pl-5 xl:pl-20'>
         <div className='grid grid-cols-1 xl:grid-cols-3 h-full w-full relative'>
           <div className='w-full h-full flex justify-start items-center h-xl:items-start h-xl:pt-24 pl-12'>
             <PersonalInfo />
           </div>
           <div
             id='content-scroll'
-            className='col-span-2 md:overflow-y-scroll md:max-h-screen pb-24'
+            className='col-span-2 md:overflow-y-scroll md:max-h-screen pb-24  pr-5 xl:pr-20'
           >
             {children}
           </div>
